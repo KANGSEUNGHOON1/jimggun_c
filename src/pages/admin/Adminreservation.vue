ae291f9178bd6385c8ba680d751ef22ebd38afad
leeseonghun <lsh0641@gmail.com>
2025-05-12 16:09:37 +0900

    관리자페이지 예약관리 진행중

--- src/pages/admin/Adminreservation.vue
+++ src/pages/admin/Adminreservation.vue
@@ -1,1 +1,31 @@
-<template>예약 관리</template>
+<template>
+  <div class="my-[30px] mx-[32px] font-bold font-[16px]">예약관리</div>
+  <div class="flex w-2/2 max-w-[1700px] mx-[32px]">
+    <ul class="flex gap-[20px] w-full">
+      <li class=" gap-[10px] w-1/4 bg-white rounded-lg shadow-[0px_4px_10px_0px_rgba(0,0,0,0.10)]">
+        <div class="w-full flex flex-col px-[30px] py-[26px]">
+          <p class="w-2/2 text-[13px] text-gray">금일 신규 예약 수</p>
+          <p class="flex items-end gap-[10px] w-full"><span class="font-bold text-lg">142 건</span><span class="text-[13px] text-manager-green pb-[2px]">+ 12</span></p>
+        </div>
+      </li>
+      <li class=" gap-[10px] w-1/4 bg-white rounded-lg shadow-[0px_4px_10px_0px_rgba(0,0,0,0.10)]">
+        <div class="w-full flex flex-col px-[30px] py-[26px]">
+          <p class="w-2/2 text-[13px] text-gray">미배차 예약 수 / 전체 예약 수</p>
+          <p class="flex items-end gap-[10px] w-full"><span class="font-bold text-lg">200</span><span class="text-[13px] pb-[2px] text-manager-orange">- 12</span> / <span class="font-bold text-lg">342 건</span></p>
+        </div>
+      </li>
+      <li class=" gap-[10px] w-1/4 bg-white rounded-lg shadow-[0px_4px_10px_0px_rgba(0,0,0,0.10)]">
+        <div class="w-full flex flex-col px-[30px] py-[26px]">
+          <p class="w-2/2 text-[13px] text-gray">금일 신규 예약 수</p>
+          <p class="flex items-end gap-[10px] w-full"><span class="font-bold text-lg">142 건</span><span class="text-[13px] text-manager-green">+ 12</span></p>
+        </div>
+      </li>
+      <li class=" gap-[10px] w-1/4 bg-white rounded-lg shadow-[0px_4px_10px_0px_rgba(0,0,0,0.10)]">
+        <div class="w-full flex flex-col px-[30px] py-[26px]">
+          <p class="w-2/2 text-[13px] text-gray">금일 신규 예약 수</p>
+          <p class="flex items-end gap-[10px] w-full"><span class="font-bold text-lg">142 건</span><span class="text-[13px] text-manager-green">+ 12</span></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
