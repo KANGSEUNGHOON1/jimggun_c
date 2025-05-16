9efea847e6ff6617d310e4ea919c804cc9fb07b0
star <byeol12284@gmail.com>
2025-05-16 17:43:52 +0900

    사이드바 , 어드민홈 다크모드 버튼

--- src/pages/admin/AdminHome.vue
+++ src/pages/admin/AdminHome.vue
@@ -1,12 +1,13 @@
 <template>
   <div
     class="max-w-[1920px] m-0 grid grid-cols-[220px_auto] grid-rows-[74px_auto] [grid-template-areas:'header_header''aside_main'] gap-y-[10px] bg-gray-100">
-    <header class="[grid-area:header] bg-white flex">
+    <header class="[grid-area:header] bg-white flex pr-[32px]">
       <div
         class="w-[220px] text-2xl flex items-center justify-center font-bold">
         Jimggun
       </div>
-      <div class="flex-1">오른쪽</div>
+      <div class="flex-1 flex items-center justify-end ">
+        <button><img src="/public/images/jung/darkbtn.png" alt="다크모드버튼"></button></div>
     </header>
 
     <aside class="[grid-area:aside] bg-white rounded-tl-10 rounded-tr-10">
